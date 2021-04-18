1c12f4fa3e7cef1d5e7c802d47f58429b525199b
Isaac Bruno Quiroga <isbrqu@gmail.com>
2021-04-18 10:19:03 -0300

    elimina la carpeta db

--- db/sql/create-table-4-videos_x_playlists.sql
+++
@@ -1,8 +0,0 @@
-create table if not exists videos_x_playlists (
-    id int(11) unsigned primary key auto_increment, 
-    video_id int(11) unsigned not null,
-    playlist_id int(11) unsigned not null,
-    foreign key (video_id) references videos (id),
-    foreign key (playlist_id) references playlists (id)
-) ENGINE = InnoDB;
-
